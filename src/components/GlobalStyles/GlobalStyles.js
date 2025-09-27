0f27387a83ad2f83fec45f5d58b4ba58037604c5
Hieu Nguyen Minh <hieu6c1@gmail.com>
2025-09-27 13:59:04 +0700

    base

---
+++ src/components/GlobalStyles/GlobalStyles.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+import './GlobalStyles.scss';
+
+function GlobalStyles({ children }) {
+    return children;
+}
+
+GlobalStyles.PropTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default GlobalStyles;
